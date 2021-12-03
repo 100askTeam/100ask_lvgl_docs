b18cd720aa990bf36a8c3a9c0d5c83aa450318f8
zhouyuebiao <837574599@qq.com>
2021-12-03 08:03:09 +0000

    change docs/conf.py: language = 'zh_CN'

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ version = subprocess.run(["../scripts/find_version.sh"], capture_output=True).st
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'zh_CN'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
