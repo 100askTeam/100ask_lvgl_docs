1ec9b799183a142de3d10ec5c3b773ada7eadc63
YobeZhou <smilezyb@163.com>
2024-05-15 03:24:16 -0400

    docs: update build_html_examples.sh

--- scripts/build_html_examples.sh
+++ scripts/build_html_examples.sh
@@ -3,13 +3,13 @@ set -e
 export PATH="/usr/lib/ccache:/usr/local/opt/ccache/libexec:$PATH"
 CURRENT_REF="$(git rev-parse HEAD)"
 rm -rf emscripten_builder
-#git clone https://github.com/lvgl/lv_sim_emscripten.git emscripten_builder
-git clone https://github.com/100askTeam/lv_web_emscripten.git emscripten_builder
+git clone https://github.com/lvgl/lv_sim_emscripten.git emscripten_builder
 scripts/genexamplelist.sh > emscripten_builder/examplelist.c
 cd emscripten_builder
+git checkout fe8cc24b991d32b3e397d2147e576d49ffca06a9 # https://github.com/lvgl/lv_web_emscripten/commits/release/v8.3/
 git submodule update --init -- lvgl
 cd lvgl
-#git checkout $CURRENT_REF
+git checkout fc98bf7f3cceb5b08a176520c0699b391628fe47 # https://github.com/lvgl/lvgl/commits/release/v8.2/
 cd ..
 git submodule update --init -- lv_drivers
 mkdir cmbuild
